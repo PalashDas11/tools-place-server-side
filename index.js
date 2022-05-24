78c694de6eacb4ed91fc3bb91f77eaaf3bb87c53
PalashDas11 <pnpalash20@gmail.com>
2022-05-24 16:49:47 +0600

    delete single oder api

--- index.js
+++ index.js
@@ -100,6 +100,14 @@ async function run() {
 
 
     })
+    // delet id 
+    app.delete('/order/:id', async (req, res)=> {
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)}
+      const result =await ordersCollection.deleteOne(query);
+      res.send(result)
+ 
+     })
     // post data on order collection 
     app.post('/order', async (req, res) => {
       const oderProduct = req.body;
@@ -115,6 +123,8 @@ async function run() {
       }
 
     })
+    
+   
 
   }
   finally {
